8f8c3379384e4d95379d2cbdd421ed3c122d3b0c
xtrembaker <xtrembaker@gmail.com>
2022-02-26 12:21:10 +0100

    Add Enable/disable DEL

---
+++ src/components/SwitchDelValue.js
@@ -0,0 +1,24 @@
+import {useState} from "react";
+import {Switch, View, Text} from "react-native";
+import {CANCELLED_STATE, sendSMS} from "../services/sendSMS";
+import {createMessageDisableDel, createMessageEnableDel} from "../services/message";
+
+export default function SwitchDelValue() {
+    const [delValue, setDelValue] = useState(false);
+    const onDelValueChange = async (value) => {
+        const message = value ? createMessageEnableDel() : createMessageDisableDel();
+        await sendSMS(message).then((response) => {
+            if(response.result === CANCELLED_STATE){
+                return;
+            }
+            setDelValue(value);
+        });
+    }
+
+    return (
+        <View style={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginLeft: 50, marginRight: 50}}>
+            <Text style={{fontSize: 18}}>Activer la LED</Text>
+            <Switch onValueChange={onDelValueChange} value={delValue}></Switch>
+        </View>
+    );
+}
--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,8 +1,9 @@
 import {sendSMS} from "../services/sendSMS";
 import {createMessageDefineMaster} from "../services/message";
 import ChangePassword from "../components/ChangePassword";
-import {View, Button} from "react-native";
+import {View, Button, Switch} from "react-native";
 import FamilyNumbersPage from "./FamilyNumbersPage";
+import SwitchDelValue from "../components/SwitchDelValue";
 
 export default function SettingsPage({navigation}){
     return (
@@ -10,6 +11,7 @@ export default function SettingsPage({navigation}){
             <Button title="Définir ce téléphone comme étant le maitre" onPress={async() => await sendSMS(createMessageDefineMaster())}/>
             <ChangePassword/>
             <Button title="Numéros famille" onPress={() => navigation.navigate('FamilyNumbersPage')}/>
+            <SwitchDelValue />
         </View>
     );
 }
