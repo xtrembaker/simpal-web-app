02c8d671f2ba0c682aa23dad74f15fa1faf8c2e6
xtrembaker <xtrembaker@gmail.com>
2022-02-27 17:25:19 +0100

    feat(family-number): add delete + local storage on family number

--- src/pages/FamilyNumbersPage.js
+++ src/pages/FamilyNumbersPage.js
@@ -1,21 +1,29 @@
-import {View, StyleSheet, TextInput} from "react-native";
-import {sendSMS} from "../services/sendSMS";
-import {createMessageAddFamilyNumber} from "../services/message";
+import {View, StyleSheet, TextInput, Button} from "react-native";
+import {CANCELLED_STATE, sendSMS} from "../services/sendSMS";
+import {
+    createMessageAddFamilyNumber,
+    createMessageDeleteFamilyNumber,
+} from "../services/message";
 import SwitchFamilyNumberAlert from "../components/SwitchFamilyNumberAlert";
+import {useEffect, useState} from "react";
+import localStorage from "../services/localStorage";
 
 const styles = StyleSheet.create({
     input: {
         borderColor: "#007AFF",
         borderWidth: 1,
         borderStyle: "dashed",
-        fontSize: 18,
+        fontSize: 20,
         marginLeft: 50,
         marginRight: 50,
         marginTop: 25,
     }
 });
 
+const STORAGE_KEY = 'familyNumbers';
+
 export default function FamilyNumbersPage(){
+    const [familyNumbers, setFamilyNumbers] = useState([]);
     const phoneNumberInputs = [
         {
             id: "15256287-612a-442a-b8b0-8fad08d0c0f1",
@@ -34,29 +42,68 @@ export default function FamilyNumbersPage(){
             placeholder: "Numéro 4"
         }];
 
-    const onSubmitPhoneNumber = async(number) =>
+    useEffect(() => {
+        localStorage().getItem(STORAGE_KEY).then((value) => {
+            if(value === null){
+                return
+            }
+            setFamilyNumbers(value);
+        })
+    }, [])
+
+    const onSubmitPhoneNumber = async(id, number) =>
     {
-        await sendSMS(createMessageAddFamilyNumber(number))
-            .then(function(response){
-                console.log(response);
+        return sendSMS(createMessageAddFamilyNumber(number)).then((response) => {
+            if(response.result === CANCELLED_STATE) {
+                return;
+            }
+            const newValue = [...familyNumbers, {id, number}];
+            return localStorage().setItem(STORAGE_KEY, newValue).then(() => {
+                setFamilyNumbers(newValue);
             })
-            .catch(function(error){
-                console.log('error', error);
-            });
+        });
+    }
+
+    const onRemovePhoneNumber = (id) => {
+        const number = findNumberInCollectionById(id)
+        if(!number){
+            return;
+        }
+        return sendSMS(createMessageDeleteFamilyNumber(number)).then(response => {
+            if(response.result === CANCELLED_STATE){
+                return;
+            }
+            const newValue = familyNumbers.filter((item) => item.id !== id);
+            localStorage().setItem(STORAGE_KEY, newValue).then(() => {
+                setFamilyNumbers(newValue);
+            })
+        })
+    }
+
+    const findNumberInCollectionById = (id) => {
+        const found = familyNumbers.find((item) => item.id === id)
+        if(!found){
+            return null;
+        }
+        return found.number;
     }
 
     return (
         <View>
             <SwitchFamilyNumberAlert />
             {phoneNumberInputs.map(({id, placeholder}) => {
-                return (<TextInput
-                    style={styles.input}
-                    key={id}
-                    placeholder={placeholder}
-                    keyboardType="phone-pad"
-                    returnKeyType="done"
-                    onSubmitEditing={({nativeEvent: {text}}) => onSubmitPhoneNumber(text)}
-                ></TextInput>)
+                return (
+                    <View key={id}>
+                        <TextInput
+                            style={styles.input}
+                            defaultValue={findNumberInCollectionById(id)}
+                            placeholder={placeholder}
+                            keyboardType="phone-pad"
+                            returnKeyType="done"
+                            onSubmitEditing={({nativeEvent: {text}}) => onSubmitPhoneNumber(id, text)}
+                        />
+                        <Button title="Supprimer" onPress={() => onRemovePhoneNumber(id)}></Button>
+                    </View>)
             })}
         </View>
     );
