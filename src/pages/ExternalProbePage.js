426085a40ccd4b689572bf7c1be3918f467f3167
xtrembaker <xtrembaker@gmail.com>
2022-02-27 18:54:23 +0100

    feat(external-probe): add temperature alert

--- src/pages/ExternalProbePage.js
+++ src/pages/ExternalProbePage.js
@@ -1,11 +1,13 @@
 import {Button, View} from "react-native";
 import {sendSMS} from "../services/sendSMS";
 import {createMessageGetProbeTemperature} from "../services/message";
+import RangeTemperatureExternalProbeAlert from "../components/RangeTemperatureExternalProbeAlert";
 
 export default function ExternalProbePage(){
     return (
         <View>
             <Button onPress={async() => { await sendSMS(createMessageGetProbeTemperature())}} title="Obtenir la temperature de la sonde"></Button>
+            <RangeTemperatureExternalProbeAlert />
         </View>
     )
 }
